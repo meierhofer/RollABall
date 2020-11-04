f008b1d6977b5076d52d7f91ddb70110de277937
k.meierhofer <karoline.meierhofer@gmail.com>
2020-11-04 13:05:59 +0100

    added some basic scripts

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+ 
+{
+    [SerializeField] private GameObject m_playerObject;
+
+    private Vector3 m_offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_offset = gameObject.transform.position - m_playerObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    gameObject.transform.position = m_playerObject.transform.position + m_offset;
+    }
+}
---
+++ Assets/Script/HelloWorld.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField] private int    m_threshold = 42;
+    private float  m_float  = 4.2f;
+    [SerializeField] private string m_message  = "Hello World";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LogHelloWorld(m_message);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LogHelloWorld(string message)
+    {
+        if(m_threshold > 10)
+            Debug.Log(message);
+    }
+}
