5e42a0c427558a1441b928f646fa32a081488e60
k.meierhofer <karoline.meierhofer@gmail.com>
2020-12-07 00:47:17 +0100

    finish much finished?

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
-        //UnityEditor.EditorApplication.ExitPlaymode();//exits the playmode in the Editor
+        UnityEditor.EditorApplication.ExitPlaymode(); //exits the playmode in the Editor
         Application.Quit();  //exit for the build game
     }
 }
