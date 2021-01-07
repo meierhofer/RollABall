aef9943615f1382e895b1d39343993860990b876
k.meierhofer <karoline.meierhofer@gmail.com>
2021-01-07 20:03:45 +0100

    added 2 player with split screen and gamepad input

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject pauseMenu, settingsMenu;
+    public GameObject menuFirstButton, pauseFirstButton, optionsFirstButton, optionsClosedButton;
+
    public void PlayGame()
     {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+
+    }
+
+    public void OpenSettings()
+    {
+        settingsMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsFirstButton);
+    }
+
+    public void CloseSettings()
+    {
+        settingsMenu.SetActive(false);
+
+        EventSystem.current.SetSelectedGameObject(null);
+        EventSystem.current.SetSelectedGameObject(optionsClosedButton);
     }
 
     public void QuitGame()
