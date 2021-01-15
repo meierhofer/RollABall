8ffc02303eec1a20a81d16c672c4deef7c5aab38
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-01-15 16:49:56 +0100

    Menu Scripts

---
+++ Assets/Scripts/PageLevel1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PageLevel1 : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.ExitPlaymode(); //exits the playmode in the Editor
+        Application.Quit();  //exit for the build game
+    }
+}
---
+++ Assets/Scripts/WinnerPage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerPage : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.ExitPlaymode(); //exits the playmode in the Editor
+        Application.Quit();  //exit for the build game
+    }
+}
