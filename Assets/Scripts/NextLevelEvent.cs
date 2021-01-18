55b1952db193dc94a96b5521600b0587ef74ab26
CarinaSt <carina.steindl@edu.fh-joanneum.at>
2021-01-18 15:29:42 +0100

    Collider next level last man standin
    funktioniert aber leider nicht

--- Assets/Scripts/NextLevelEvent.cs
+++ Assets/Scripts/NextLevelEvent.cs
@@ -9,7 +9,7 @@ public class NextLevelEvent : MonoBehaviour
     {
         if (ChangeScene.gameObject.CompareTag("player"))
         {
-            SceneManager.LoadScene("PageLevel1"); //2 is the PageNextLevel
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //is the PageNextLevel
         }
     }
 }
